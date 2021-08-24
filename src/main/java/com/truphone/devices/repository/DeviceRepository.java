a07b114bf790f2d554d1e4563c1b7aa0e3971e99
Joel Figueiredo <joel.figueiredo@isobar.com>
2021-08-24 23:31:51 +0100

    Add repository DeviceRepository
    Add DeviceRepository that implements JPA

---
+++ src/main/java/com/truphone/devices/repository/DeviceRepository.java
@@ -0,0 +1,9 @@
+package com.truphone.devices.repository;
+
+import com.truphone.devices.model.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+    Device findDeviceByBrand(String brand);
+}
